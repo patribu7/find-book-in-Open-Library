452f3966990b00adf8ce1596a468688d85739ab4
Patrizia Busatto <patribu7@gmail.com>
2023-09-01 13:40:33 +0200

    Aggiunto un template in html

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import component from './component';
 
-document.body.appendChild(component());
 
 import './style.css';
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,41 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, './src/app.js'),
-      },
-      output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'deploy')
-      },
-      devServer: {
-        static: './deploy',
-        open: true
-      },
+  entry: {
+    main: path.resolve(__dirname, './src/app.js'),
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'deploy'),
+    clean: true
+  },
+  devServer: {
+    static: './deploy',
+    open: true
+  },
 
-    module: {
+  module: {
     rules: [
-        { 
-        test: /\.css$/, 
-        use: ["style-loader", "css-loader"] 
-        },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
 
-        { 
+      {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
         type: 'asset/resource',
-        },
+      },
     ]
-    },
+  },
 
   plugins: [
     new HtmlWebpackPlugin({
       title: "trova libri in Open Library",
+      template: path.resolve(__dirname, "./src/template.html"),
+      filename: 'index.html',
     }),
     new CleanWebpackPlugin()
   ],
