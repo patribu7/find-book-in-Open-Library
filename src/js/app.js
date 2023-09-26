76d43847e0c58185bbc4c1a5a55e699781aa0bc6
Patrizia Busatto <patribu7@gmail.com>
2023-09-26 13:10:20 +0200

    For deploy

--- src/js/app.js
+++ src/js/app.js
@@ -7,23 +7,9 @@ import '../css/style.css';
 
 // Import our custom CSS
 import '../scss/styles.scss';
+
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap';
-
-
-
-
-
-
-
-
-
-// esempi per importare solo alcuni moduli da bootstrap
-
-// import Alert from 'bootstrap/js/dist/alert';
-
-// // or, specify which plugins you need:
-// import { Tooltip, Toast, Popover } from 'bootstrap'
 
 
 //----------------------------//
