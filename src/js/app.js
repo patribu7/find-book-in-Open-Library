dc897d146744ffb63966ae3755bc4e859de5be6c
Patrizia Busatto <patribu7@gmail.com>
2023-09-27 11:25:51 +0200

    Aggiornata la gestione dell'errore

--- src/js/app.js
+++ src/js/app.js
@@ -17,7 +17,6 @@ import * as filter from "./filters";
 import { value_search, value_type } from "./getInputValue";
 import researchType from './researchType';
 import { SearchParameters, setProperty, BtnScroll, Card, Placeholder } from "./component";
-import { error } from 'jquery';
 
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
@@ -25,47 +24,52 @@ const cardsPlace = document.getElementById('cards-place');
 const warning = document.getElementById('warning');
 
 function executeSearch() {
-    
-        cardsPlace.innerHTML = '';
-        warning.innerHTML = '';
 
-        let placeholder = new Placeholder();
-        let research = new SearchParameters(value_type(), value_search());
-        filter.report.fill(`trovati 0 libri`);
-        placeholder.create()
-            .then(() => research.get())
-            .then(library => setProperty(library))
-            .then(library => {
-                filter.report.fill(`trovati ${library.count} libri`);
-                if (!library.count) {
-                    warning.innerHTML = `
+    cardsPlace.innerHTML = '';
+    warning.innerHTML = '';
+
+    let placeholder = new Placeholder();
+    let research = new SearchParameters(value_type(), value_search());
+    filter.report.fill(`trovati 0 libri`);
+    placeholder.create()
+        .then(() => research.get())
+        .then(library => setProperty(library))
+        .then(library => {
+            filter.report.fill(`trovati ${library.count} libri`);
+            if (!library.count) {
+                warning.innerHTML = `
                     The search has no results. Try searching for a valid ${researchType[value_type()].type}
                     in the page <a href = 'https://openlibrary.org${researchType[value_type()].urlToSite}'
                     target ='_blank'> Open Library </a>`;
 
+                $('#btn-scroll').remove()
+            } else {
+                let scrolling = new BtnScroll();
+                if ($('#btn-scroll') != null) {
                     $('#btn-scroll').remove()
+                    scrolling.create()
+                    $('#btn-scroll').on('click', () => scrolling.showOthers(research))
                 } else {
-                    let scrolling = new BtnScroll();
-                    if ($('#btn-scroll') != null) {
-                        $('#btn-scroll').remove()
-                        scrolling.create()
-                        $('#btn-scroll').on('click', () => scrolling.showOthers(research))
-                    } else {
-                        scrolling.create();
-                        $('#btn-scroll').on('click', () => scrolling.showOthers(research));
+                    scrolling.create();
+                    $('#btn-scroll').on('click', () => scrolling.showOthers(research));
 
-                    };
-                    library.forEach(book => {
-                        let card = new Card(book);
-                        card.create();
-                    })
-                }
-                placeholder.remove()
+                };
+                library.forEach(book => {
+                    let card = new Card(book);
+                    card.create();
+                })
+            }
+            placeholder.remove()
 
-            })
-            .catch((error) => {
-                alert(error + '<br> charset not allowed');
-                placeholder.remove()
+        })
+        .catch((error) => {
+            
+            console.error(error);
+            alert('Charset not allowed')
+                    ;
+
+        
+            placeholder.remove()
         })
 }
 
